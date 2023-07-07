d6f182ec83a8a01221ea229aa197f6dc850a828d
Jcodefox <jcodefox@gmail.com>
2023-07-07 17:22:23 -0400

    Change gravity globally instead of in an Area2D

--- scripts/gravity_controller.gd
+++ scripts/gravity_controller.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-@onready var gravity_area: Area2D = get_node("Gravity")
 @onready var gravity_slider: Slider = get_node("CanvasLayer/GravitySlider")
 
 func _ready():
 	gravity_slider.value = (gravity_slider.max_value + gravity_slider.min_value) / 2
 
 func _process(delta):
-	gravity_area.gravity_direction.y = -gravity_slider.value
+	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, -gravity_slider.value * 980)
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-@onready var gravity_area: Area2D = get_node("../Gravity")
-
 var walk_speed: float = 70
 var walk_left: bool = false
+var gravity: float
 
 func _process(delta):
+	gravity = PhysicsServer2D.area_get_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY)
 	if is_on_wall():
 		walk_left = get_wall_normal().x < -0.5
 		$Sprite.flip_h = walk_left
 	if is_on_floor():
 		velocity.x = walk_speed * (-1 if walk_left else 1)
-	velocity += delta * gravity_area.gravity_direction * gravity_area.gravity
+	velocity.y += delta * gravity
 	move_and_slide()
