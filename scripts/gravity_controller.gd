57b906b7b5110e8554847d4d5bbcd87bb6cdb95f
Jcodefox <jcodefox@gmail.com>
2023-07-07 16:35:13 -0400

    Auto center slider

--- scripts/gravity_controller.gd
+++ scripts/gravity_controller.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var gravity_area: Area2D = get_node("Gravity")
 @onready var gravity_slider: Slider = get_node("CanvasLayer/GravitySlider")
 
+func _ready():
+	gravity_slider.value = (gravity_slider.max_value + gravity_slider.min_value) / 2
+
 func _process(delta):
-	gravity_area.gravity_direction.y = -gravity_slider.value + 5
+	gravity_area.gravity_direction.y = -gravity_slider.value
 
