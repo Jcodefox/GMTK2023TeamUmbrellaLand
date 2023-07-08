09a210bcaeb6eff46b3561f59b85bdc73ed1a63f
Jcodefox <jcodefox@gmail.com>
2023-07-08 01:25:30 -0400

    Started work on transition to play_as_game

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,41 +1,42 @@
 extends CharacterBody2D
 
 @onready var levels: Array[String] = [
-	"main", "final_win_screen"
+	"main", "main", "final_win_screen"
 ]
 
-var walk_speed: float = 70
+@export var jump_force: float = 275
+@export var walk_speed: float = 70
+
 var walk_left: bool = false
-var gravity: float
+@onready var gravity: float = PhysicsServer2D.area_get_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY)
+var should_jump: bool = false
 
 func _process(delta):
-	gravity = PhysicsServer2D.area_get_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY)
-	if is_on_wall():
-		walk_left = get_wall_normal().x < -0.5
-		$Sprite.flip_h = walk_left
-	velocity.x = walk_speed * (-1 if walk_left else 1)
+	velocity.x = walk_speed
 	velocity.y += delta * gravity
+	
+	if should_jump and is_on_floor():
+		velocity.y -= jump_force
+		should_jump = false
+	else:
+		should_jump = false
 	
 	move_and_slide()
 
 func _on_win_check_body_entered(body):
-	var tween: Tween = get_tree().create_tween()
-	tween.tween_property($Sprite, "modulate", Color(1,1,1,0), 1)
-	tween.tween_interval(0.1)
-	tween.tween_callback(go_to_next_level)
+	go_to_next_level()
 
 func go_to_next_level():
 	var current_level_path: String = get_tree().current_scene.scene_file_path
 	var current_level_index: int = levels.find(current_level_path)
 	get_tree().change_scene_to_file("res://scenes/" + levels[current_level_index + 1] + ".tscn")
 
-func _on_lose_check_body_entered(body):
-	var tween1: Tween = get_tree().create_tween()
-	var tween2: Tween = get_tree().create_tween()
-	tween1.tween_property($Camera2D, "zoom", Vector2(20, 20), 1)
-	tween2.tween_property($Camera2D, "rotation", 4 * PI, 1)
-	tween2.tween_interval(0.1)
-	tween2.tween_callback(restart_level)
+func _on_hole_check_body_exited(body):
+	if get_tree().get_frame() < 16:
+		return
+	if not $HoleCheck.has_overlapping_bodies() and is_on_floor():
+		should_jump = true
 
-func restart_level():
-	get_tree().change_scene_to_file(get_tree().current_scene.scene_file_path)
+func _on_enemy_and_wall_check_body_entered(body):
+	if is_on_floor():
+		should_jump = true
